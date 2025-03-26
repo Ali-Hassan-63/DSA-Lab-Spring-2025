1a77492fd8e869522e74b16d397277f81901ef94
Syed Ali Hassan <165467417+Ali-Hassan-63@users.noreply.github.com>
2025-03-26 15:47:07 +0500

    Add files via upload

---
+++ Week-6/Exercise 6.2/Source.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<stdlib.h>
+#include<conio.h>
+#include"LinkedList.cpp"
+using namespace std;
+int main() {
+	List<int>L;
+	int len;
+	L.push(10);
+	L.push(20);
+	L.push(40);
+	L.push(50);
+	L.deleteafter(40);
+	L.insertafter(10, 30);
+	L.deleteItem(50);
+	while (!L.emptylist()) {
+		cout << L.pop() << endl;
+	}
+	string str,reversedstr;
+	cout << "Enter a string to reverse it = ";
+	cin >> str;
+	for (int i = 0; i < str.length(); i++) {
+		L.push(str[i]);
+	}
+	for (int i = 0; i < str.length(); i++) {
+		reversedstr+=L.pop();
+	}
+	cout << "\nReversed string = " << reversedstr;
+	_getch();
+	return 0;
+}
