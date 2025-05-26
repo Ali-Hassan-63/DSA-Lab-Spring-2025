1bfc0b40b566acaa18bbd60baf48cbbcc345e3b9
Syed Ali Hassan <165467417+Ali-Hassan-63@users.noreply.github.com>
2025-05-26 20:34:16 +0500

    Update Source.cpp

--- Week-11/Source.cpp
+++ Week-11/Source.cpp
@@ -1,139 +1,157 @@
-//#include <iostream>
-//#include <vector>
-//#include <queue>
-//using namespace std;
-//
-//class Graph {
-//private:
-//    int V; // Number of vertices
-//    vector<vector<int>> adj; // Adjacency list
-//
-//public:
-//    Graph(int v) {
-//        V = v;
-//        adj.resize(V);
-//    }
-//
-//    void addEdge(int u, int v) {
-//        adj[u].push_back(v); // Directed edge
-//    }
-//
-//    void DFSUtil(int v, vector<bool>& visited) {
-//        visited[v] = true;
-//        cout << v << " ";
-//
-//        for (int neighbor : adj[v]) {
-//            if (!visited[neighbor]) {
-//                DFSUtil(neighbor, visited);
-//            }
-//        }
-//    }
-//
-//    void DFS(int start) {
-//        vector<bool> visited(V, false);
-//        cout << "DFS starting from vertex " << start << ": ";
-//        DFSUtil(start, visited);
-//        cout << endl;
-//    }
-//
-//    void BFS(int start) {
-//        vector<bool> visited(V, false);
-//        queue<int> q;
-//
-//        visited[start] = true;
-//        q.push(start);
-//
-//        cout << "BFS starting from vertex " << start << ": ";
-//        while (!q.empty()) {
-//            int v = q.front();
-//            q.pop();
-//            cout << v << " ";
-//
-//            for (int neighbor : adj[v]) {
-//                if (!visited[neighbor]) {
-//                    visited[neighbor] = true;
-//                    q.push(neighbor);
-//                }
-//            }
-//        }
-//        cout << endl;
-//    }
-//};
-//
-//int main() {
-//    Graph g(6);
-//    g.addEdge(0, 1);
-//    g.addEdge(0, 2);
-//    g.addEdge(1, 3);
-//    g.addEdge(1, 4);
-//    g.addEdge(2, 5);
-//
-//    g.DFS(0);
-//    g.BFS(0);
-//
-//    return 0;
-//}
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Graph {
+private:
+    int V; // Number of vertices
+    vector<vector<int>> adj; // Adjacency list
+
+public:
+    Graph(int v) {
+        V = v;
+        adj.resize(V);
+    }
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(v); // Directed edge
+    }
+
+    void DFSUtil(int v, vector<bool>& visited) {
+        visited[v] = true;
+        cout << v << " ";
+
+        for (int neighbor : adj[v]) {
+            if (!visited[neighbor]) {
+                DFSUtil(neighbor, visited);
+            }
+        }
+    }
+
+    void DFS(int start) {
+        vector<bool> visited(V, false);
+        cout << "DFS starting from vertex " << start << ": ";
+        DFSUtil(start, visited);
+        cout << endl;
+    }
+
+    void BFS(int start) {
+        vector<bool> visited(V, false);
+        queue<int> q;
+
+        visited[start] = true;
+        q.push(start);
+
+        cout << "BFS starting from vertex " << start << ": ";
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            cout << v << " ";
+
+            for (int neighbor : adj[v]) {
+                if (!visited[neighbor]) {
+                    visited[neighbor] = true;
+                    q.push(neighbor);
+                }
+            }
+        }
+        cout << endl;
+    }
+};
+
+int main() {
+    Graph g(6);
+    g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(1, 3);
+    g.addEdge(1, 4);
+    g.addEdge(2, 5);
+
+    g.DFS(0);
+    g.BFS(0);
+
+    return 0;
+}
+//////////////////////////////////////////////////////////////////
 #include <iostream>
 #include <vector>
 #include <queue>
 #include <climits>
 using namespace std;
 
-class DijkstraGraph {
-private:
-    int V;
-    vector<vector<pair<int, int>>> adj; // pair<vertex, weight>
+// Define a pair type for (distance, vertex)
+typedef pair<int, int> pii;
+
+class Graph {
+    int V; // Number of vertices
+    vector<vector<pii>> adj; // Adjacency list: (neighbor, weight)
 
 public:
-    DijkstraGraph(int v) {
-        V = v;
+    Graph(int vertices) {
+        V = vertices;
         adj.resize(V);
     }
 
-    void addEdge(int u, int v, int w) {
-        adj[u].push_back({ v, w }); // Directed weighted edge
+    void addEdge(int u, int v, int weight) {
+        adj[u].push_back({ v, weight });
+        adj[v].push_back({ u, weight }); // For undirected graph
     }
 
-    void dijkstra(int src) {
-        vector<int> dist(V, INT_MAX);
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<>> pq;
+    void dijkstra(int start) {
+        vector<int> dist(V, INT_MAX); // Distance to each vertex
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
 
-        dist[src] = 0;
-        pq.push({ 0, src });
+        dist[start] = 0;
+        pq.push({ 0, start }); // (distance, vertex)
 
         while (!pq.empty()) {
             int u = pq.top().second;
+            int d = pq.top().first;
             pq.pop();
 
-            for (auto [v, w] : adj[u]) {
-                if (dist[u] + w < dist[v]) {
-                    dist[v] = dist[u] + w;
+            // Visit all neighbors
+            for (auto neighbor : adj[u]) {
+                int v = neighbor.first;
+                int weight = neighbor.second;
+
+                if (dist[v] > d + weight) {
+                    dist[v] = d + weight;
                     pq.push({ dist[v], v });
                 }
             }
         }
 
-        cout << "Shortest distances from source " << src << ":\n";
-        for (int i = 0; i < V; ++i) {
+        // Print shortest distances
+        cout << "Shortest distances from vertex " << start << ":\n";
+        for (int i = 0; i < V; i++) {
             if (dist[i] == INT_MAX)
-                cout << "To " << i << " = INF" << endl;
+                cout << "Vertex " << i << ": Unreachable\n";
             else
-                cout << "To " << i << " = " << dist[i] << endl;
+                cout << "Vertex " << i << ": " << dist[i] << "\n";
         }
     }
 };
 
 int main() {
-    DijkstraGraph g(6);
-    g.addEdge(0, 1, 4);
-    g.addEdge(0, 2, 1);
-    g.addEdge(2, 1, 2);
-    g.addEdge(1, 3, 1);
-    g.addEdge(2, 3, 5);
-    g.addEdge(3, 4, 3);
-    g.addEdge(4, 5, 1);
+    int V, E;
+    cout << "Enter number of vertices and edges: ";
+    cin >> V >> E;
 
-    g.dijkstra(0);
+    Graph g(V);
+
+    cout << "Enter edges in format (u v weight):\n";
+    for (int i = 0; i < E; i++) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        g.addEdge(u, v, w);
+    }
+
+    int start;
+    cout << "Enter starting vertex for Dijkstra: ";
+    cin >> start;
+
+    g.dijkstra(start);
 
     return 0;
 }
